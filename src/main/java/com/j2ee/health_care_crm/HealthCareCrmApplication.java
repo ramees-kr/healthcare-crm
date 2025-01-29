195ece54d1ddc8fe34a232c586699a1569774b2a
GaurangHD <gdhameliya2000@gmail.com>
2025-01-28 22:34:17 -0500

    first commit

---
+++ src/main/java/com/j2ee/health_care_crm/HealthCareCrmApplication.java
@@ -0,0 +1,13 @@
+package com.j2ee.health_care_crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthCareCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthCareCrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/j2ee/health_care_crm/HealthCareCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.j2ee.health_care_crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthCareCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
